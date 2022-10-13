a311ed5f390dc72e5c200324183c5c53159e97e7
Ivan Mikhalin <mikhalin_ivan@mail.ru>
2022-10-13 16:21:58 +0400

    initial commit

---
+++ src/test/java/PostTest.java
@@ -0,0 +1,32 @@
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import org.hamcrest.Matcher;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.Matchers.*;
+import java.util.Objects;
+
+import static io.restassured.RestAssured.given;
+
+public class PostTest {
+
+
+    @Test
+    void shouldCheckSomething() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+            ;
+
+    }
+    
+}
+
+
+
+// http://localhost:9999/api/v1/demo/accounts
